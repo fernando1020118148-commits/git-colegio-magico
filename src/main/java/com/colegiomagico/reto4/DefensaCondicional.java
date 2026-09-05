8173b39c6c5f301825bc62c9e0b715ee8a1ea273
fernando1020118148-commits <fernando1020118148@gmail.com>
2026-09-05 16:45:22 +0000

    feat: reto 3 completado con exito

--- src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
+++ src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
@@ -6,7 +6,11 @@ package com.colegiomagico.reto4;
 public class DefensaCondicional {
 
     public static String evaluar(String hechizo) {
-        return ""; // TODO: Cambia esto
+        if (hechizo=="Imperius" || hechizo=="Crucio") {
+            return "Maldicion";
+        } else {
+                return "Permitido";
+        } // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
